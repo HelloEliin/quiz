966aa110a43f5f499f54de3884adb9349b50c900
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-10-19 09:24:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,79 +7,85 @@ import '../css/app.css'
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
- import axios from 'axios';
- window.axios = axios;
+import axios from 'axios';
+window.axios = axios;
 
- window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-
- const startButton = document.getElementById('startButton')
- let questions = document.getElementById('questions')
- const getAnswerBtn = document.getElementById('getAnswerBtn')
- const yesBtn = document.getElementById('yesBtn')
- const noBtn = document.getElementById('noBtn')
- const oneMoreTimeBtn = document.getElementById('oneMoreTimeBtn')
- const startView = document.getElementById('startView')
- const questionView = document.getElementById('questionView').style.display="none";
- const answerView = document.getElementById('answerView').style.display="none";
- const vector = document.getElementById('whiteDonkey').style.display="block";
-
- const scoreView = document.getElementById('scoreView').style.display="none";
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
- let currentQuestion = 0;
- let score = 0;
- let questionCounter = 0;
- const MAX_QUESTIONS = 35;
+const startButton = document.getElementById('startButton')
+let questions = document.getElementById('questions')
+const getAnswerBtn = document.getElementById('getAnswerBtn')
+const yesBtn = document.getElementById('yesBtn')
+const noBtn = document.getElementById('noBtn')
+const oneMoreTimeBtn = document.getElementById('oneMoreTimeBtn')
+const startView = document.getElementById('startView')
+const questionView = document.getElementById('questionView').style.display = "none";
+const answerView = document.getElementById('answerView').style.display = "none";
+const vector = document.getElementById('whiteDonkey').style.display = "block";
 
+const scoreView = document.getElementById('scoreView').style.display = "none";
+
+
+let currentQuestion = 0;
+let score = 0;
+let questionCounter = 0;
+
+
+
+let allQuestions;
+let category;
 
 
 
 startButton.addEventListener('click', startGame)
 
+
 function startGame() {
 
-       $(function () {
-        $.getJSON("/questions", function (data) {
-            let currentQuestion = 0;
-    
-            const allQuestions = Object.values(data);
-            const category = Object.keys(data[0]);
+    $(function () {
+     $.getJSON("/questions", function (data) {
+         let currentQuestion = 0;
+ 
+        allQuestions = Object.values(data);
+        category = Object.keys(data[0]);
 
-            const yes = Object.keys(allQuestions[0, 1]);
-            console.log(yes);
-            console.log(category);
-            console.log(allQuestions);
-            let hey = Object.entries(allQuestions[0, 1]);
+         const yes = Object.keys(allQuestions[0, 1]);
+         console.log(yes);
+         console.log(category);
+         console.log(allQuestions);
+         let hey = Object.entries(allQuestions[0, 1]);
 
-            
-            question.innerText = hey[1];
-
-   
-           
-            
-            
-        })
-    });
+         
+         question.innerText = hey[1];
+ 
+         
+     })
+ });
 
 
-    document.getElementById('startView').style.display = 'none';
-    document.getElementById('questionView').style.display = "block";
-    document.getElementById('scoreView').style.display = "none";
+ document.getElementById('startView').style.display = 'none';
+ document.getElementById('questionView').style.display = "block";
+ document.getElementById('scoreView').style.display = "none";
 
 
 
 }
 
+
+
+
 getAnswerBtn.addEventListener('click', showAnswer)
 
-function showAnswer(){
+function showAnswer() {
     document.body.style.backgroundColor = "#7678ED";
-    document.getElementById('questionView').style.display="none";
-    document.getElementById('whiteDonkey').style.display='none';
-    document.getElementById('vector').style.display='none';
-    document.getElementById('answerView').style.display="block";
+    document.getElementById('questionView').style.display = "none";
+    document.getElementById('whiteDonkey').style.display = 'none';
+    document.getElementById('vector').style.display = 'none';
+    document.getElementById('answerView').style.display = "block";
+
     
+
 
 
 }
@@ -89,30 +95,23 @@ function showAnswer(){
 yesBtn.addEventListener('click', ifRightAnswer)
 
 
-function ifRightAnswer(){
-    
+function ifRightAnswer() {
+
     document.body.style.backgroundColor = "#ffffff";
-    document.getElementById('answerView').style.display="none";
-    document.getElementById('scoreView').style.display="block";
-    document.getElementById('vector').style.display="block";
-    document.getElementById('questionView').style.display="none";
-    document.getElementById('whiteDonkey').style.display="block";
+    document.getElementById('answerView').style.display = "none";
+    document.getElementById('scoreView').style.display = "block";
+    document.getElementById('vector').style.display = "block";
+    document.getElementById('questionView').style.display = "none";
+    document.getElementById('whiteDonkey').style.display = "block";
 
 
 
-
- fetch('http://localhost:3000/storage/questions.json')
- .then((response) => response.json())
-.then((data) => console.log(data));
-
-
-
-
+        SetNewQuestion();
 
 
 
     score++
-    
+
 
 }
 
@@ -122,14 +121,14 @@ function ifRightAnswer(){
 noBtn.addEventListener('click', ifWrongAnswer)
 
 
-function ifWrongAnswer(){
-    
+function ifWrongAnswer() {
+
     document.body.style.backgroundColor = "#ffffff";
-    document.getElementById('answerView').style.display="none";
-    document.getElementById('scoreView').style.display="block";
-    document.getElementById('vector').style.display="block";
-    document.getElementById('questionView').style.display="none";
-    document.getElementById('whiteDonkey').style.display="block";
+    document.getElementById('answerView').style.display = "none";
+    document.getElementById('scoreView').style.display = "block";
+    document.getElementById('vector').style.display = "block";
+    document.getElementById('questionView').style.display = "none";
+    document.getElementById('whiteDonkey').style.display = "block";
 
 
 
@@ -142,18 +141,19 @@ function ifWrongAnswer(){
 oneMoreTimeBtn.addEventListener('click', oneMoreRound)
 
 
-function oneMoreRound(){
+function oneMoreRound() {
 
 
     questionCounter = 0;
     score = 0;
 
-    document.getElementById('startView').style.display="block";
-    document.getElementById('answerView').style.display="none";
-    document.getElementById('scoreView').style.display="block";
-    document.getElementById('vector').style.display="block";
-    document.getElementById('questionView').style.display="none";
-    document.getElementById('whiteDonkey').style.display="block";
+    document.getElementById('startView').style.display = "block";
+    document.getElementById('answerView').style.display = "none";
+    document.getElementById('scoreView').style.display = "block";
+    document.getElementById('vector').style.display = "block";
+    document.getElementById('questionView').style.display = "none";
+    document.getElementById('whiteDonkey').style.display = "block";
+    document.getElementById('scoreView').style.display = "none";
 
 
 
