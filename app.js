97c6bc379c0645e28bf2424385eda1294b891ee1
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-10-21 17:23:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,115 @@ import '../css/app.css'
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+
+import { spline } from '@georgedoescode/spline';
+import { createNoise2D } from 'simplex-noise';
+
+// our <path> element
+const path = document.querySelector('path');
+// used to set our custom property values
+const root = document.documentElement;
+
+const blobLeft = document.getElementById("blobleft")
+const blobRight = document.getElementById("blobright")
+const blobRightWhite = document.getElementById("blobrightwhite")
+const blobLeftWhite = document.getElementById("blobleftwhite")
+
+function createPoints() {
+    const points = [];
+    // how many points do we need
+    const numPoints = 6;
+    // used to equally space each point around the circle
+    const angleStep = (Math.PI * 2) / numPoints;
+    // the radius of the circle
+    const rad = 75;
+
+    for (let i = 1; i <= numPoints; i++) {
+        // x & y coordinates of the current point
+        const theta = i * angleStep;
+
+        const x = 100 + Math.cos(theta) * rad;
+        const y = 100 + Math.sin(theta) * rad;
+
+        // store the point
+        points.push({
+            x: x,
+            y: y,
+            /* we need to keep a reference to the point's original {x, y} coordinates 
+            for when we modulate the values later */
+            originX: x,
+            originY: y,
+            // more on this in a moment!
+            noiseOffsetX: Math.random() * 1000,
+            noiseOffsetY: Math.random() * 1000,
+        });
+    }
+
+    return points;
+}
+
+
+const points = createPoints();
+
+function map(n, start1, end1, start2, end2) {
+    return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2
+}
+
+const simplex = new createNoise2D()
+
+let noiseStep = 0.0025;
+
+function noise(x, y) {
+    return simplex(x, y);
+};
+
+
+
+
+(function animate() {
+
+    blobLeft.setAttribute('d', spline(points, 1, true));
+    blobRight.setAttribute('d', spline(points, 1, true));
+
+    blobRightWhite.setAttribute('d', spline(points, 1, true));
+    blobLeftWhite.setAttribute('d', spline(points, 1, true));
+
+    requestAnimationFrame(animate);
+
+    for (let i = 0; i < points.length; i++) {
+        const point = points[i]
+
+        const nX = noise(point.noiseOffsetX, point.noiseOffsetX)
+        const nY = noise(point.noiseOffsetY, point.noiseOffsetY)
+
+        const x = map(nX, -1, 1, point.originX - 5, point.originX + 5)
+        const y = map(nY, -1, 1, point.originY - 10, point.originY + 10)
+
+        point.x = x
+        point.y = y
+
+        point.noiseOffsetX += noiseStep
+        point.noiseOffsetY += noiseStep
+    }
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import axios from 'axios';
 window.axios = axios;
 
@@ -30,6 +139,7 @@ const counter2 = document.getElementById('counter2')
 const progressBar = document.getElementById('progressBar')
 const progressBar2 = document.getElementById('progressBar2')
 const categories = document.getElementById('category')
+
 const movieScore = document.getElementById("movies").children
 const sportsScore = document.getElementById("sports").children
 const geographScore = document.getElementById("geograph").children
@@ -51,6 +161,7 @@ let progressWidth = 0;
 let progressWidth2 = 0;
 
 
+
 let correctMovies = [];
 let correctSports = [];
 let correctScience = [];
@@ -58,6 +169,10 @@ let correctHistory = [];
 let correctMusic = [];
 let correctOther = [];
 let correctGeoraph = [];
+
+let allCorrectScores = [correctGeoraph, correctHistory, correctMovies,
+     correctMusic, correctOther, correctScience, correctSports]
+let allIndex = [movieScore, scienceScore, otherScore, geographScore, musicScore, historyScore, sportsScore]
 
 
 
@@ -153,24 +268,25 @@ yesBtn.addEventListener('click', ifRightAnswer)
 function ifRightAnswer() {
 
 
-
-    if (allQuestions[questionCounter]['category'] === 'Film & TV') {
+    if (categories.textContent === 'Film & TV') {
 
         movieScore[correctMovies.length].classList.remove("bg-lightGrey");
         movieScore[correctMovies.length].classList.add("bg-lightGreen");
 
-        correctMovies.push(1)   
+        correctMovies.push(1)
 
-    }else if (allQuestions[questionCounter]['category'] === 'Historia') {
+    }
 
-        movieScore[correctHistory.length].classList.remove("bg-lightGrey");
-        movieScore[correctHistory.length].classList.add("bg-lightGreen");
+    if (categories.textContent === 'Historia') {
+
+        historyScore[correctHistory.length].classList.remove("bg-lightGrey");
+        historyScore[correctHistory.length].classList.add("bg-lightGreen");
 
         correctHistory.push(1)
 
     }
 
-    else if (allQuestions[questionCounter]['category'] === 'Vetenskap') {
+    if (categories.textContent === 'Vetenskap') {
 
         scienceScore[correctScience.length].classList.remove("bg-lightGrey");
         scienceScore[correctScience.length].classList.add("bg-lightGreen");
@@ -179,7 +295,7 @@ function ifRightAnswer() {
 
     }
 
-    else if (allQuestions[questionCounter]['category'] === 'Sport') {
+    if (categories.textContent === 'Sport') {
 
         sportsScore[correctSports.length].classList.remove("bg-lightGrey");
         sportsScore[correctSports.length].classList.add("bg-lightGreen");
@@ -187,7 +303,7 @@ function ifRightAnswer() {
         correctSports.push(1)
 
     }
-    else if (allQuestions[questionCounter]['category'] === 'Geografi') {
+    if (categories.textContent === 'Geografi') {
 
         geographScore[correctGeoraph.length].classList.remove("bg-lightGrey");
         geographScore[correctGeoraph.length].classList.add("bg-lightGreen");
@@ -197,7 +313,7 @@ function ifRightAnswer() {
     }
 
 
-    else if (allQuestions[questionCounter]['category'] === 'Övrigt') {
+    if (categories.textContent === 'Övrigt') {
 
         otherScore[correctOther.length].classList.remove("bg-lightGrey");
         otherScore[correctOther.length].classList.add("bg-lightGreen");
@@ -206,11 +322,17 @@ function ifRightAnswer() {
 
     }
 
+    if (categories.textContent ==='Musik') {
+
+        musicScore[correctMusic.length].classList.remove("bg-lightGrey");
+        musicScore[correctMusic.length].classList.add("bg-lightGreen");
+
+        correctMusic.push(1)
+
+    }
 
 
-
-
-
+    console.log(allCorrectScores)
 
     document.body.style.backgroundColor = "#ffffff";
     document.getElementById('answerView').style.display = "none";
@@ -265,6 +387,25 @@ oneMoreTimeBtn.addEventListener('click', oneMoreRound)
 
 
 function oneMoreRound() {
+
+
+    for (var i = 0; i<allCorrectScores.length; i++) {
+        allCorrectScores[i].splice(0);
+     }
+
+     for (var i = 0; i < movieScore.length; i++) {
+        movieScore[i].style.backgroundColor = "#EEEEEE";
+        sportsScore[i].style.backgroundColor = "#EEEEEE";
+        scienceScore[i].style.backgroundColor = "#EEEEEE";
+        historyScore[i].style.backgroundColor = "#EEEEEE";
+        sportsScore[i].style.backgroundColor = "#EEEEEE";
+        otherScore[i].style.backgroundColor = "#EEEEEE";
+        geographScore[i].style.backgroundColor = "#EEEEEE"; 
+        musicScore[i].style.backgroundColor = "#EEEEEE"; 
+    }
+
+
+
 
     questionCounter = 0;
     score = 0;
